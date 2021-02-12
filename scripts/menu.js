c7212dc5cb64807c1ab74d7a7431267faefed136
TitouanSans <Titouan.sanshochart@gmail.com>
2021-02-12 09:23:43 +0100

    Work in progress version

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,23 @@
+// Menu nav complet
+
+document.getElementsByClassName('tmp-menu')[0].innerHTML = `
+<a href="index.html" class="home-link">Titouan Sans - Développeur Front-End</a>
+<div class="site-links">
+    <div class="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <a href="index.html" class="active">Accueil</a>
+    <!-- <a href="profil.html">Présentation</a> -->
+    <!-- <a href="competences.html">Compétences</a> -->
+    <a href="images/competences/CV_TitouanSans_2021.pdf" target="_blank">Compétences</a>
+    <a href="portfolio.html">Projets</a>
+    <a href="images/competences/CV_TitouanSans_2021.pdf" target="_blank">CV</a>
+    <!-- <a href="contact.html">Contact</a> -->
+</div>
+`;
+
 // Menu.js : bascule de l'attribut data-state du bouton burger du menu au clic (opened/closed)
 
 var menu = document.querySelector('.tmp-menu');
